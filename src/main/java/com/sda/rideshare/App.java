5b9cc9b24201a9a368f9102ea4601e1a1824b5fb
Miriam <miriamcrina@yahoo.com>
2020-12-14 21:43:11 +0200

    test

---
+++ src/main/java/com/sda/rideshare/App.java
@@ -0,0 +1,13 @@
+package com.sda.rideshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sda/rideshare/AppTest.java
@@ -0,0 +1,20 @@
+package com.sda.rideshare;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
